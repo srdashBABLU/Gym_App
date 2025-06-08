ab246351e9fcde2f6c2d68c6d97ea632d5408ac2
srdashBABLU <srdashbablu143@gmail.com>
2025-06-08 21:21:12 +0530

    optimised the app

--- app/src/main/java/com/xash/gymapp/MainActivity.kt
+++ app/src/main/java/com/xash/gymapp/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.xash.gymapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
